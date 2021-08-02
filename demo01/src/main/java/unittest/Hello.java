baaa3250cc92d7ce5fd933562f6cbf4ad076ba57
Supoj Ploytaveechai <supoj.pl@kbtg.tech>
2021-08-02 14:34:14 +0700

    Success with fail case

--- demo01/src/main/java/unittest/Hello.java
+++ demo01/src/main/java/unittest/Hello.java
@@ -4,4 +4,16 @@ public class Hello {
     public String hi(String name) {
         return "Hello, " + name;
     }
+
+    UserDB userDB;
+
+    public String workWithDb(int id) {
+        return userDB.getNameById(id);
+    }
+}
+
+class UserDB { //connect to DB
+    public String getNameById(int id) {
+        throw new RuntimeException("TODO");
+    }
 }
--- demo01/src/test/java/unittest/HelloTest.java
+++ demo01/src/test/java/unittest/HelloTest.java
@@ -17,4 +17,33 @@ class HelloTest {
         // Assert == Validate with expected result
         assertEquals("Hello, supoj", actualResult);
     }
+
+    @Test
+    @DisplayName("Test with database ( I = Isolated/Independent)")
+    public void case02() {
+        Hello hello = new Hello();
+        hello.userDB = new UserDB() {
+            @Override
+            public String getNameById(int id) {
+                return "somkiat";
+            }
+        };
+        String name = hello.workWithDb(1);
+        assertEquals("somkiat", name);
+    }
+
+    @Test
+    @DisplayName("Exception when id = 2")
+    public void case03() {
+        Hello hello = new Hello();
+        hello.userDB = new UserDB() {
+            @Override
+            public String getNameById(int id) {
+                throw new UserNotFoundException("Id=" + id + " not found");
+            }
+        };
+
+        Exception exception = assertThrows(UserNotFoundException.class, () -> hello.workWithDb(2));
+        assertEquals("Id=2 not found", exception.getMessage());
+    }
 }
