6aebca5030d039ea3d57d44d3b1e635d3956eca1
Supoj Ploytaveechai <supoj.pl@kbtg.tech>
2021-08-03 15:17:29 +0700

    Use bean from spring boot

--- workshop-dip/src/main/java/com/example/workshopdip/WorkshopDipApplication.java
+++ workshop-dip/src/main/java/com/example/workshopdip/WorkshopDipApplication.java
@@ -10,6 +10,10 @@ public class WorkshopDipApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(WorkshopDipApplication.class, args);
 		System.out.println("Bean count=" + context.getBeanDefinitionCount());
+
+		GenerateIdService service = context.getBean(GenerateIdService.class);
+		String id = service.get();
+		System.out.println("ID=" + id);
 	}
 
 }
