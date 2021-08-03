adad3338349599b709b89a0cff0fee3a9747b2d3
Supoj Ploytaveechai <supoj.pl@kbtg.tech>
2021-08-03 16:31:48 +0700

    Testing unit test + mockito

--- workshop-dip/src/test/java/com/example/workshopdip/GenerateIdServiceUnitTest.java
+++ workshop-dip/src/test/java/com/example/workshopdip/GenerateIdServiceUnitTest.java
@@ -2,9 +2,9 @@ package com.example.workshopdip;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Random;
 
@@ -12,23 +12,25 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest
-public class GenerateIdServiceTest {
+@ExtendWith(MockitoExtension.class)
+public class GenerateIdServiceUnitTest {
 
-    @Autowired
-    private GenerateIdService generateIdService;
-
-    @MockBean
+    @Mock
     private Random random;
 
     @Test
-    @DisplayName("เริ่มต้นทดสอบกับ spring boot test")
+    @DisplayName("เริ่มต้นทดสอบกับ mockito + junit 5")
     public void case01() {
         // Arrange
         when(random.nextInt(anyInt())).thenReturn(7);
+        GenerateIdService generateIdService = new GenerateIdService();
+        generateIdService.setRandom(random);
+
         // Act
         String id = generateIdService.get();
+
         // Assert
         assertEquals("XYZ7", id);
     }
+
 }
