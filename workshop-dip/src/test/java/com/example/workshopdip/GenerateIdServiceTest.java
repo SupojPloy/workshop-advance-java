4d5361a71ae49fffc1cca55be596b5b058077ef7
Supoj Ploytaveechai <supoj.pl@kbtg.tech>
2021-08-03 15:57:24 +0700

    Create stub random with Mockito

--- workshop-dip/src/test/java/com/example/workshopdip/GenerateIdServiceTest.java
+++ workshop-dip/src/test/java/com/example/workshopdip/GenerateIdServiceTest.java
@@ -4,8 +4,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class GenerateIdServiceTest {
@@ -13,10 +17,17 @@ class GenerateIdServiceTest {
     @Autowired
     private GenerateIdService generateIdService;
 
+    @MockBean
+    private Random random;
+
     @Test
     @DisplayName("เริ่มต้นทดสอบกับ spring boot test")
     public void case01() {
+        // Arrange
+        when(random.nextInt(10)).thenReturn(7);
+        // Act
         String id = generateIdService.get();
+        // Assert
         assertEquals("XYZ7", id);
     }
 }
