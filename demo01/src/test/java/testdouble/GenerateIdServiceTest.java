b27cab3d5e57d2a1b69635123fb517a813b9187a
Supoj Ploytaveechai <supoj.pl@kbtg.tech>
2021-08-03 14:19:19 +0700

    Demo with Stub

--- demo01/src/test/java/testdouble/GenerateIdServiceTest.java
+++ demo01/src/test/java/testdouble/GenerateIdServiceTest.java
@@ -3,15 +3,32 @@ package testdouble;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GenerateIdServiceTest {
 
     @Test
-    @DisplayName("ID = XYZ7")
+    @DisplayName("Demo Stub Random :: ID = XYZ7")
     public void case01() {
         GenerateIdService service = new GenerateIdService();
+        service.setRandom(new StubRandom(7));
         String id = service.get();
         assertEquals("XYZ7", id);
+    }
+}
+
+class StubRandom extends Random {
+
+    private final int number;
+
+    public StubRandom(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public int nextInt(int bound) {
+        return this.number;
     }
 }
